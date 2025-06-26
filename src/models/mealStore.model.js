3a682194fd53fb461e2cbe51b77895a1f40859ab
Rashid-Mahmood501 <rashidmahmood4434429@gmail.com>
2025-06-26 12:50:29 +0500

    timestamps

--- src/models/mealStore.model.js
+++ src/models/mealStore.model.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
-const mealSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  type: {
-    type: String,
-    enum: ["breakfast", "lunch", "dinner", "snack"],
-    required: true,
+const mealSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    type: {
+      type: String,
+      enum: ["breakfast", "lunch", "dinner", "snack"],
+      required: true,
+    },
+    image: { type: String },
+    calories: { type: Number, required: true },
+    protein: { type: Number, required: true },
+    fat: { type: Number, required: true },
+    carbs: { type: Number, required: true },
   },
-  image: { type: String },
-  calories: { type: Number, required: true },
-  protein: { type: Number, required: true },
-  fat: { type: Number, required: true },
-  carbs: { type: Number, required: true },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("mealStore", mealSchema);
