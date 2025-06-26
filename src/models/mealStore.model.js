adb51b6b8b5976d1426c1677a1e60f6c569936e1
Rashid-Mahmood501 <rashidmahmood4434429@gmail.com>
2025-06-26 13:06:02 +0500

    Revert "timestamps"
    This reverts commit 3a682194fd53fb461e2cbe51b77895a1f40859ab.

--- src/models/mealStore.model.js
+++ src/models/mealStore.model.js
@@ -1,22 +1,17 @@
 const mongoose = require("mongoose");
 
-const mealSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    type: {
-      type: String,
-      enum: ["breakfast", "lunch", "dinner", "snack"],
-      required: true,
-    },
-    image: { type: String },
-    calories: { type: Number, required: true },
-    protein: { type: Number, required: true },
-    fat: { type: Number, required: true },
-    carbs: { type: Number, required: true },
+const mealSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  type: {
+    type: String,
+    enum: ["breakfast", "lunch", "dinner", "snack"],
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  image: { type: String },
+  calories: { type: Number, required: true },
+  protein: { type: Number, required: true },
+  fat: { type: Number, required: true },
+  carbs: { type: Number, required: true },
+});
 
 module.exports = mongoose.model("mealStore", mealSchema);
